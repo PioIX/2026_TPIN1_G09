190cfd138dd5ecb6751bc7af7616885ebb2869a3
lucio-18 <lrosenthal@pioix.edu.ar>
2026-07-13 09:02:51 -0300

    Update mysql.js

--- backend/modulos/mysql.js
+++ backend/modulos/mysql.js
@@ -1,1 +1,22 @@
 // Conexion MySQL
+const mysql=require("mysql2");
+require("dotenv").config();
+
+const conexion=mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
+});
+
+conexion.connect((error)=>{
+
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Base conectada");
+    }
+
+});
+
+module.exports=conexion;
