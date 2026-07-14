f154262baaf3e03fb68353478a88d0c42321942c
lolobeccaria <lbeccaria@pioix.edu.ar>
2026-07-13 22:40:20 -0300

    img nombres correctos

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -5,133 +5,133 @@ let jugadores = [
         equipo: "Inter Miami",
         posicion: "Delantero",
         valor: 18000000,
-        imagen: "public/jugador1.jpg"
+        imagen: "img/messi.jpg"
     },
     {
         nombre: "Kylian Mbappé",
         equipo: "Real Madrid",
         posicion: "Delantero",
         valor: 180000000,
-        imagen: "public/jugador2.jpg"
+        imagen: "img/mbappe.png"
     },
     {
         nombre: "Erling Haaland",
         equipo: "Manchester City",
         posicion: "Delantero",
         valor: 180000000,
-        imagen: "public/haaland.jpg"
+        imagen: "img/haaland.jpg"
     },
     {
         nombre: "Jude Bellingham",
         equipo: "Real Madrid",
         posicion: "Mediocampista",
         valor: 180000000,
-        imagen: "public/bellingham.jpg"
+        imagen: "img/bellingham.jpg"
     },
     {
         nombre: "Vinícius Júnior",
         equipo: "Real Madrid",
         posicion: "Delantero",
         valor: 150000000,
-        imagen: "public/vinicius.jpg"
+        imagen: "img/vinicius.jpg"
     },
     {
         nombre: "Lamine Yamal",
         equipo: "Barcelona",
         posicion: "Delantero",
         valor: 180000000,
-        imagen: "public/yamal.jpg"
+        imagen: "img/yamal.jpg"
     },
     {
         nombre: "Bukayo Saka",
         equipo: "Arsenal",
         posicion: "Delantero",
         valor: 140000000,
-        imagen: "public/saka.jpg"
+        imagen: "img/saka.jpg"
     },
     {
         nombre: "Phil Foden",
         equipo: "Manchester City",
         posicion: "Mediocampista",
         valor: 130000000,
-        imagen: "public/foden.jpg"
+        imagen: "img/foden.jpg"
     },
     {
         nombre: "Robert Lewandowski",
         equipo: "Barcelona",
         posicion: "Delantero",
         valor: 15000000,
-        imagen: "public/lewandowski.jpg"
+        imagen: "img/lewandowski.jpg"
     },
     {
         nombre: "Kevin De Bruyne",
         equipo: "Napoli",
         posicion: "Mediocampista",
         valor: 20000000,
-        imagen: "public/debruyne.jpg"
+        imagen: "img/debruyne.jpg"
     },
     {
         nombre: "Mohamed Salah",
         equipo: "Liverpool",
         posicion: "Delantero",
         valor: 50000000,
-        imagen: "public/salah.jpg"
+        imagen: "img/salah.jpg"
     },
     {
         nombre: "Virgil van Dijk",
         equipo: "Liverpool",
         posicion: "Defensor",
         valor: 25000000,
-        imagen: "public/vandijk.jpg"
+        imagen: "img/vandijk.jpg"
     },
     {
         nombre: "Thibaut Courtois",
         equipo: "Real Madrid",
         posicion: "Arquero",
         valor: 35000000,
-        imagen: "public/courtois.jpg"
+        imagen: "img/courtois.jpg"
     },
     {
         nombre: "Rodri Hernández",
         equipo: "Manchester City",
         posicion: "Mediocampista",
         valor: 110000000,
-        imagen: "public/rodri.jpg"
+        imagen: "img/rodri.jpg"
     },
     {
         nombre: "Florian Wirtz",
         equipo: "Liverpool",
         posicion: "Mediocampista",
         valor: 130000000,
-        imagen: "public/wirtz.jpg"
+        imagen: "img/wirtz.jpg"
     },
     {
         nombre: "Jamal Musiala",
         equipo: "Bayern Múnich",
         posicion: "Mediocampista",
         valor: 130000000,
-        imagen: "public/musiala.jpg"
+        imagen: "img/musiala.jpg"
     },
     {
         nombre: "Pedri González",
         equipo: "Barcelona",
         posicion: "Mediocampista",
         valor: 120000000,
-        imagen: "public/pedri.jpg"
+        imagen: "img/pedri.jpg"
     },
     {
         nombre: "Julián Álvarez",
         equipo: "Atlético Madrid",
         posicion: "Delantero",
         valor: 90000000,
-        imagen: "public/alvarez.jpg"
+        imagen: "img/alvarez.jpg"
     },
     {
         nombre: "Declan Rice",
         equipo: "Arsenal",
         posicion: "Mediocampista",
         valor: 110000000,
-        imagen: "public/rice.jpg"
+        imagen: "img/rice.jpg"
     }
 ];
 
